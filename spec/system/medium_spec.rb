e2d1d6fca3daa7eed705c7f78d9b83b948fc571f
Yamashita <yk.emoemo@gmail.com>
2021-06-08 13:01:11 +0900

    bug on preview image

---
+++ spec/system/medium_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe 'medium', type: :system do
+  describe '画像ファイルをアップロード' do
+    context '画像ファイルをアップロードせずにプレビューページを閲覧または公開' do
+      it '表示されること' do
+      end
+    end
+    context '画像ファイルをアップロードしてプレビューページを閲覧または公開' do
+      it '表示されること' do
+      end
+  end
+end
