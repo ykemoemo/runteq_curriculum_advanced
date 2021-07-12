349c24acdc2d5083608fb36e2421d0048143fbf0
ykemoemo <ykemoemo@gmail.com>
2021-07-12 15:05:40 +0900

    mail summary

--- app/controllers/admin/site/attachments_controller.rb
+++ app/controllers/admin/site/attachments_controller.rb
@@ -1,8 +1,8 @@
 class Admin::Site::AttachmentsController < ApplicationController
   def destroy
-	authorize(current_site)
-	image = ActiveStorage::Attachment.find(params[:id])
-	image.purge
-	redirect_to edit_admin_site_path
+    authorize(current_site)
+    image = ActiveStorage::Attachment.find(params[:id])
+    image.purge
+    redirect_to edit_admin_site_path
   end
 end
